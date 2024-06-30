901a4d45077ac59bc88332d22f4ae7d8915439f2
mini <155148431+mini0192@users.noreply.github.com>
2024-07-01 05:35:27 +0900

    UPDATE: TestController update2

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping
     public String test() {
-        return "Good TEST 123123123123";
+        return "제발 좀 깔끔하게 되라..";
     }
 }
