05302292a0de1ee76ffaf8cf9d62c623a10c70a7
mini <155148431+mini0192@users.noreply.github.com>
2024-07-03 04:18:56 +0900

    Update: gradle파일 swagger 확인 및 controller 확인 작업 마무리

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -1,12 +1,24 @@
 package com.example.demo;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/users")
+@Tag(name = "Test", description = "음냐")
 public class TestController {
     @GetMapping
+    @Operation(description = "해치웠나?")
     public String test() {
+        return "제발 좀 깔끔하게 되라..123";
+    }
+
+    @GetMapping("/test")
+    @Operation(description = "됐나..?")
+    public String test1() {
         return "제발 좀 깔끔하게 되라..123";
     }
 }
